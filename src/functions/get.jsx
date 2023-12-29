0327dde806853e33d14168ee3696ed39ccb904d5
Huzaifa Tawab <huzaifatawab33@gmail.com>
2023-12-29 19:14:16 +0500

    env

--- src/functions/get.jsx
+++ src/functions/get.jsx
@@ -1,12 +1,12 @@
 const axios = require("axios");
 
-function Get() {
+function Get(uri) {
   // `${process.env.API_KEY}`
   let response = "hello";
   let config = {
     method: "get",
     maxBodyLength: Infinity,
-    url: "https://api.b365api.com/v3/events/inplay?token=179024-3d6U7zylacO78f&sport_id=1",
+    url: `https://api.b365api.com/v3/${uri}`,
     headers: {},
   };
 
