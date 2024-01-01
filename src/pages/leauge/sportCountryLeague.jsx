0550452886de723e4a250b9897b74d97c892033a
aksamariyam8 <aqsa.mariyam97@gmail.com>
2024-01-01 19:06:21 +0500

    sportcountryleague page updated

--- src/pages/leauge/sportCountryLeague.jsx
+++ src/pages/leauge/sportCountryLeague.jsx
@@ -1,12 +1,20 @@
-import React, { useState, useEffect } from "react";
+import React, { useRef, useState, useEffect } from "react";
+import NavBar from "../../components/Navbar";
+import Sidebar from "../../components/Sidebar";
+import RightSidebar from "../../components/RightSidebar";
 import axios from "axios";
 import { Route, Link, Routes, useParams } from "react-router-dom";
 import { Flex, Img, Stack, Text, Box, HStack } from "@chakra-ui/react";
-
 import sports from "../../json/sports.json";
 import SportsIMG from "../../json/sportsImg";
 import BookMakerLight from "../../components/BookMakerLight";
-const SportCountryLeague = () => {
+function SportCountryLeague() {
+  const [mobileView, setMobileView] = useState(window.innerWidth < 700);
+
+  const handleResize = () => {
+    setMobileView(window.innerWidth < 700);
+  };
+
   const [events, setEvents] = useState([]);
   const [sport, setCurrentSport] = useState(null);
 
@@ -66,129 +74,159 @@ const SportCountryLeague = () => {
   const getFlagUrl = (countryCode) =>
     `https://flagsapi.com/${countryCode.toUpperCase()}/flat/32.png`;
 
+  useEffect(() => {
+    const debouncedHandleResize = debounce(handleResize, 200);
+
+    window.addEventListener("resize", debouncedHandleResize);
+
+    return () => {
+      window.removeEventListener("resize", debouncedHandleResize);
+    };
+  }, []);
+
+  function debounce(func, wait) {
+    let timeout;
+
+    return function executedFunction(...args) {
+      const later = () => {
+        clearTimeout(timeout);
+        func(...args);
+      };
+
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+    };
+  }
+
   return (
-    <>
-      {params && (
-        <>
-          <div>
-            {params["sport"]} {">"} {params["country"]} {">"}
-            {params["league"]}
-          </div>
-          <div>{params["league"]} Betting Odds</div>
-        </>
-      )}
-      {sport && (
-        <>
-          <div>
-            <Img src={SportsIMG[sport.Name]} /> {params["sport"]} /
-            <Img src={SportsIMG[sport.Name]} /> {params["country"]}/
-            {params["league"]}
-          </div>
-        </>
-      )}
-      {events &&
-        events.map((event) => {
-          console.log(event);
-          return (
-            <div key={event.id}>
-              <Stack gap={"15px"} margin={"10px"}>
-                <Flex justifyContent={"space-between"}>
-                  <Text
-                    textAlign={"center"}
-                    padding={"8px 15px"}
-                    borderRadius={"30px"}
-                    color={"#656EF5"}
-                    bg={"#656FF513"}
-                    fontSize={"13px"}
-                    textStyle={"medium"}
-                  >
-                    {new Date(event.time * 1000).toLocaleTimeString()}
-                  </Text>
-                </Flex>
-                <HStack
-                  border={"1px solid #656EF5"}
-                  h={"80px"}
-                  borderRadius={"8px"}
-                  padding={"5px"}
-                  justifyContent={"space-between"}
-                  alignItems={"center"}
-                  margin={"10px 0px 10px 0px"}
-                >
-                  <Flex gap={"10px"} marginLeft={"20px"}>
-                    <Text
-                      gap={"5px"}
-                      display={"flex"}
-                      alignItems={"center"}
-                      fontSize={"16px"}
-                      textStyle={"medium"}
-                    >
-                      {event.home.name}
-                      <Img src={getTeamUrl(event.home.image_id)} />
-                    </Text>
-                    <Text
-                      textStyle={"bold"}
-                      fontSize={"16px"}
-                      color={"#656EF5"}
-                    >
-                      0 : 2{" "}
-                    </Text>
-                    <Text
-                      gap={"5px"}
-                      display={"flex"}
-                      alignItems={"center"}
-                      fontSize={"16px"}
-                      textStyle={"medium"}
-                    >
-                      <Img src={getTeamUrl(event.away.image_id)} />
-                      {event.away.name}
-                    </Text>
-                  </Flex>
-                  <Flex gap={"5px"}>
-                    <BookMakerLight id={1} per={2.82} />
-                    <BookMakerLight id={1} per={2.82} />
-                    <BookMakerLight id={1} per={2.82} />
-                    <BookMakerLight id={1} per={2.82} />
-                  </Flex>
-                </HStack>
-              </Stack>
-            </div>
-          );
-        })}
-    </>
-    // <ChakraProvider>
-    //   <Box p="4">
-    //     <h1>Football League Table</h1>
-    //     {loading ? (
-    //       <CircularProgress isIndeterminate color="teal.500" />
-    //     ) : (
-    //       <Table variant="striped" colorScheme="teal" style={{ width: "100%" }}>
-    //         <Thead>
-    //           <Tr>
-    //             {columns.map((column) => (
-    //               <Th key={column.Header}>{column.Header}</Th>
-    //             ))}
-    //           </Tr>
-    //         </Thead>
-    //         <Tbody>
-    //           {tableData.map((row) => (
-    //             <Tr key={row.pos}>
-    //               {columns.map((column) => (
-    //                 <Td key={column.Header}>
-    //                   {column.accessor.includes(".")
-    //                     ? row[column.accessor.split(".")[0]][
-    //                         column.accessor.split(".")[1]
-    //                       ]
-    //                     : row[column.accessor]}
-    //                 </Td>
-    //               ))}
-    //             </Tr>
-    //           ))}
-    //         </Tbody>
-    //       </Table>
-    //     )}
-    //   </Box>
-    // </ChakraProvider>
+    <HStack
+      gap={"0px"}
+      justifyContent={"start"}
+      alignItems={"start"}
+      bg={"#F8F8FF"}
+    >
+      {mobileView ? <></> : <Sidebar sportsList={sports} />}
+      <Box w={mobileView ? "100vw" : "80vw"}>
+        <NavBar />
+        <HStack alignItems={"end"}>
+          <Box
+            w={mobileView ? "100%" : "80%"}
+            overflowY={"scroll"}
+            h={"85vh"}
+            css={{
+              "&::-webkit-scrollbar": {
+                width: "4px",
+              },
+              "&::-webkit-scrollbar-track": {
+                width: "6px",
+              },
+              "&::-webkit-scrollbar-thumb": {
+                background: "#656EF5",
+                borderRadius: "24px",
+              },
+            }}
+          >
+            <>
+              {params && (
+                <>
+                  <div>
+                    {params["sport"]} {">"} {params["country"]} {">"}
+                    {params["league"]}
+                  </div>
+                  <div>{params["league"]} Betting Odds</div>
+                </>
+              )}
+              {sport && (
+                <>
+                  <div>
+                    <Img src={SportsIMG[sport.Name]} /> {params["sport"]} /
+                    <Img src={SportsIMG[sport.Name]} /> {params["country"]}/
+                    {params["league"]}
+                  </div>
+                </>
+              )}
+              {events &&
+                events.map((event) => {
+                  console.log(event);
+                  return (
+                    <div key={event.id}>
+                      <Stack gap={"15px"} margin={"10px"}>
+                        <Flex justifyContent={"space-between"}>
+                          <Text
+                            textAlign={"center"}
+                            padding={"8px 15px"}
+                            borderRadius={"30px"}
+                            color={"#656EF5"}
+                            bg={"#656FF513"}
+                            fontSize={"13px"}
+                            textStyle={"medium"}
+                          >
+                            {new Date(event.time * 1000).toLocaleTimeString()}
+                          </Text>
+                        </Flex>
+                        <HStack
+                          border={"1px solid #656EF5"}
+                          h={"80px"}
+                          borderRadius={"8px"}
+                          padding={"5px"}
+                          justifyContent={"space-between"}
+                          alignItems={"center"}
+                          margin={"10px 0px 10px 0px"}
+                        >
+                          <Flex gap={"10px"} marginLeft={"20px"}>
+                            <Text
+                              gap={"5px"}
+                              display={"flex"}
+                              alignItems={"center"}
+                              fontSize={"16px"}
+                              textStyle={"medium"}
+                            >
+                              {event.home.name}
+                              <Img src={getTeamUrl(event.home.image_id)} />
+                            </Text>
+                            <Text
+                              textStyle={"bold"}
+                              fontSize={"16px"}
+                              color={"#656EF5"}
+                            >
+                              0 : 2{" "}
+                            </Text>
+                            <Text
+                              gap={"5px"}
+                              display={"flex"}
+                              alignItems={"center"}
+                              fontSize={"16px"}
+                              textStyle={"medium"}
+                            >
+                              <Img src={getTeamUrl(event.away.image_id)} />
+                              {event.away.name}
+                            </Text>
+                          </Flex>
+                          <Flex gap={"5px"}>
+                            <BookMakerLight id={1} per={2.82} />
+                            <BookMakerLight id={1} per={2.82} />
+                            <BookMakerLight id={1} per={2.82} />
+                            <BookMakerLight id={1} per={2.82} />
+                          </Flex>
+                        </HStack>
+                      </Stack>
+                    </div>
+                  );
+                })}
+            </>
+          </Box>
+          {/* Right side */}
+          {mobileView ? (
+            <></>
+          ) : (
+            <Box w={"20%"} h={"100%"} maxW={"300px"}>
+              <RightSidebar />
+            </Box>
+          )}
+        </HStack>
+      </Box>
+    </HStack>
   );
-};
+}
 
 export default SportCountryLeague;
